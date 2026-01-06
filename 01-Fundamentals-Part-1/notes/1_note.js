6b27b23f28c1addc1c7d308c585621b9c519648a
Dar Rehan Rasool <95292119+darrehanrasool@users.noreply.github.com>
2026-01-06 16:34:31 +0530

    Dar Rehan Rasool
    sycomusiclover

---
+++ 01-Fundamentals-Part-1/notes/1_note.js
@@ -0,0 +1,13 @@
+// command + F for quick replacement of words names for testing
+// command + option + j for javascript developer console
+// alert() or window.alert() is a funtion in javascript that is used for dispalying pop ups
+window.alert("Dar Rehan Rasool");
+alert("Dar Rehan Rasool");
+console.log("it will log the string to the output console");
+// console.log is not meant for user its meant for developer to debug the code.
+let variable = 45;
+// let is a keywoard for creating variables in javascript
+// in javascript we === for Equality
+if (variable === 45) alert("hi buddy");
+// prompt() is a funtion in javascript that is used to take the input from the user
+profile_name = prompt("Enter the name buddy \n");
---
+++ 01-Fundamentals-Part-1/notes/2_note.js
@@ -0,0 +1,16 @@
+// javascript is a language that powers the web. developed by brendan eich in 10 days .it was first called live script
+// different version of javascript ,europian standise the language they created ECMA script europian computer manifacture
+// association.script is a peice of code that tell the html element how to behave.
+// javascript is a highlevel ,object oriented, multi paradigm prgraming language
+// High Level : Actually we don't have to worry about complex memory management system like microprocessor
+// Object oriented : its based on objects
+// Multi paradigm : we can use different style of programing ie monolithic,imparative,declarative programing style
+// HTML for Markup layout of website <p></p> Noun
+// CSS for styling the website p{color:red}  Adjective
+// Javascript for funtionality p.hide().     Verbs
+// javascript allows us to add dynamic effects to the websites ie spinner of twiter
+// we can run javascript outside the web Browser ie on web server via Node.js
+// Node.js allows us to write backend applications
+// We can use javascript to buld native mobile applications as well as native desktop applications
+// Native Mobile Applications ie via Reactnative and ionic and Native deskhtop applications Via javascript electron
+// Javascript Releases ES5,ES6,ES7,ES8,ES9,ES10,ES11.son on,WE USE ECMA SCRIPT 6 MODULE SYNTAX IE ES6 Modern Javascript
