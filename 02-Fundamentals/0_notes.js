b729e7a331322cf2df3b7298197330e94a252ba3
Dar Rehan Rasool <darrehanrasool@gmail.com>
2026-02-03 15:19:42 +0530

    Reactjs development
    ES6 module syntax

--- 02-Fundamentals/0_notes.js
+++ 02-Fundamentals/0_notes.js
@@ -1,7 +1,8 @@
-// BMI Calculator
+"use strict";
+// bmi calculator dummy ! example
 function bmiCalculator(weight, height) {
   return Math.round(weight / Math.pow(height, 2));
-  // return Math.floor(weight / height ** 2);
+  //return Math.floor(weight / height ** 2);
 }
 let bmi = bmiCalculator(65, 1.8);
 console.log(bmi);
