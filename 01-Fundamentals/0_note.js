bce220b3cf9a9aff3dd6693f24c3f677cdacd7d9
Dar Rehan Rasool <darrehanrasool@gmail.com>
2026-02-03 15:20:33 +0530

    Javascript development
    ES6 module syntax

--- 01-Fundamentals/0_note.js
+++ 01-Fundamentals/0_note.js
@@ -1,29 +1,3 @@
-/*
-javascript is a programing language that powers the whole web. developed by brendan eich in 10 days .it was first called live script
-different version of javascript ,europian standised the language they created ECMA script europian computer manifacture
-association.script is a peice of code that tell the html element how to behave.
-javascript is a highlevel ,object oriented, multi paradigm programing language
-High Level : Actually we don't have to worry about complex memory management system like microprocessor assembly language
-Object oriented : its based on objects
-Multi paradigm : we can use different style of programing ie monolithic, imparative , declarative programing style
-HTML for Markup layout of website <p> </p> Noun
-CSS for styling the website p{color:red}  Adjective
-Javascript for funtionality p.hide().     Verbs
-javascript allows us to add dynamic effects to the websites ie spinner of twiter
-we can run javascript outside the web Browser ie on web server via Node.js
-Node.js allows us to write backend applications
-We can use javascript to build native mobile applications as well as native desktop applications
-Native Mobile Applications ie via Reactnative + ionic and Native desktop applications Via javascript + electron
-Javascript Releases ES5,ES6,ES7,ES8,ES9,ES10,ES11.so.....on,WE USE ECMA SCRIPT 6 MODULE SYNTAX IE Modern Javascript
-alert() or window.alert() is a funtion in javascript that is used for dispalying pop ups.
-its a keywoard or funtion whose meaning is already known by the interpreter 
-console.log is not meant for user. user can't see it ,its meant for developer to debug the code.
-console.log("it will log the string to the output console");
-interpreted programing language javascript python ruby
-compiled programing language c c++ java swift
-Babel is a javascript compiler it allows us to convert ES6 module syntax ie Modern javascript into ES5 module syntax ie
-standard javascript for browser compatablity transpiling ES6 to standrad javascript ES5
-*/
 // Assignment 1
 let country = "india";
 let district = "kashmere";
