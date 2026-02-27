2d8fcbd078621f8a457b439d85806041ba0dabc6
Dar Rehan Rasool <darrehanrasool@gmail.com>
2026-02-27 21:25:44 +0530

    دار ریحان رسول
     دار ریحان رسول

---
+++ 05-Guess MyNumber/note.js
@@ -0,0 +1,6 @@
+document.querySelector('.message').textContent;
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+document.querySelector('.guess').value = 23;
+document.querySelector('.guess').value;
